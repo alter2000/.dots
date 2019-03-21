0b24faca36532bfa13287ddeb59d29967e7bb30e
Jor Bashllari <altnate2000@gmail.com>
2019-03-21 20:35:37 +0100

    lol idk

--- zsh/vars.zsh
+++ zsh/vars.zsh
@@ -16,12 +16,13 @@ export CLICOLOR=1
 
 # colored-man-pages from gh/imkira
 export LESS_TERMCAP_mb=$'\E[01;31m'
-export LESS_TERMCAP_md=$'\E[01;38;5;74m'
+export LESS_TERMCAP_md=$'\E[01;33;5;74m'
 export LESS_TERMCAP_me=$'\E[0m'
 export LESS_TERMCAP_se=$'\E[0m'
-export LESS_TERMCAP_so=$'\E[38;33;246m'
+export LESS_TERMCAP_so=$'\E[01;44;246m'
 export LESS_TERMCAP_ue=$'\E[0m'
-export LESS_TERMCAP_us=$'\E[04;38;5;146m'
+export LESS_TERMCAP_us=$'\E[01;32m'
+# export LESS_TERMCAP_us=$'\E[04;38;5;146m'
 
 ### HISTORY + SUBSTRING SEARCH ###
 HISTSIZE=10000
