f7f04c5d68f81e1fdb6c4d47601904d83c12426e
Jor Bashllari <altnate2000@gmail.com>
2019-05-04 06:52:29 +0200

    remove deprecated options and other bs changes

--- i3/focus.py
+++ i3/focus.py
@@ -2,19 +2,23 @@
 
 import i3
 
+
 def find_tmp():
     tmps = i3.filter(nodes=[], window=None, name=None)
     if len(tmps) != 1:
         return None
     return tmps[0]['id']
 
+
 def create_tmp(current_id):
     i3.focus(con_id=current_id)
     i3.split('vertical')
     i3.open()
 
+
 def destroy_tmp(tmp_id):
     i3.kill(con_id=tmp_id)
+
 
 def make_float(current_id):
     i3.focus(con_id=current_id)
@@ -22,25 +26,24 @@ def make_float(current_id):
     i3.resize('set 1000 1000')
     i3.move('position center')
 
+
 def make_unfloat(current_id):
     i3.focus(con_id=current_id)
     i3.floating('disable')
 
-def main():
+
+if __name__ == "__main__":
     current = i3.filter(nodes=[], focused=True)[0]
     current_id = current['id']
     if "on" in current['floating']:
         tmp_id = find_tmp()
-        if tmp_id != None:
+        if tmp_id is not None:
             i3.focus(con_id=tmp_id)
             make_unfloat(current_id)
             destroy_tmp(tmp_id)
         else:
             make_unfloat()
     else:
-        if find_tmp() == None:
+        if find_tmp() is None:
             create_tmp(current_id)
             make_float(current_id)
-
-
-main()
