5464577619a058b1cfb67a48ef64a95f86381e96
Jor Bashllari <altnate2000@gmail.com>
2019-05-26 22:49:00 +0000

    polybar: fix launch script

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -6,10 +6,14 @@ pkill polybar
 # Wait until the processes have been shut down
 while pgrep -x polybar >/dev/null; do sleep 0.2; done
 
+[ -n "$1" ] && bar="$1" || bar="i3bar1"
 # honestly idk why i stole this
 # [[ $(pgrep i3) -gt 0 ]] && export PB_WM_RESTACK=i3
 
+CONMON=$(command -v xrandr && xrandr || exit 1 | grep ' connected ' | head -n 1 | cut -d' ' -f1)
+
 # Launch bars
-polybar $1 -c ~/.dots/polybar/config &
+MONITOR=${CONMON-"eDP-1"} polybar "$bar" -c ~/.dots/polybar/config \
+    >/tmp/polybar.log 2>&1 &
 
 echo "Bars launched..."
--- polybar/reddit.sh
+++ polybar/reddit.sh
@@ -13,5 +13,9 @@ icon=' '
 unreadColor='#e06c75'
 readColor='#e0e0e0'
 
-unread=$(curl -s -A "Linux:com.ss132ikl.polybarinbox:v1.0.0 by /u/132ikl" $url | jq '.["data"]["children"] | length ')
-(( "$unread" > "0" )) && echo "%{F$unreadColor}$icon $unread" || echo "%{F$readColor}$icon $unread"
+unread=$(curl -s -A "Linux:com.ss132ikl.polybarinbox:v1.0.0 by /u/132ikl" \
+        $url \
+        | jq '.["data"]["children"] | length ')
+(( "$unread" > "0" )) \
+    && echo "%{F$unreadColor}$icon $unread" \
+    || echo "%{F$readColor}$icon $unread"
