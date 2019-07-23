19bd1708d527367d5fad7a090e8a2203232bbcab
Jor Bashllari <altnate2000@gmail.com>
2019-07-23 21:53:28 +0200

    scripts: notifi.py kinda works

--- scripts/notifi.py
+++ scripts/notifi.py
@@ -1,14 +1,24 @@
 #!/usr/bin/env python3
 
-from os import environ as env, listdir
+from os import environ as env, listdir as ls, popen
+from os.path import isfile
 import datetime
 import subprocess
 import atexit
 try:
     from psutil import sensors_battery
 except ImportError:
+    class Battery(object):
+        def __init__(self, pc: float, pp: bool):
+            self.percent: float = pc
+            self.power_plugged: bool = pp
+
     def sensors_battery() -> str:
-        return ""
+        with open('/sys/class/power_supply/BAT0/capacity') as nfd, \
+             open('/sys/class/power_supply/BAT0/power_now') as sfd:
+            ncur = float(nfd.readlines()[0])
+            state = (sfd.readlines()[0] == 1)
+        return Battery(ncur, state)
 
 MAILPATH = env['MAILPATH'] if env['MAILPATH'] else env['MAIL']
 # TODO
@@ -40,13 +50,12 @@ def battery() -> str:
     if battery.power_plugged:
         icon = '{0}'.format(icon_charging)
     else:
-        icon = '{0}'.format(icons[round(
-            charge / 100 * (len(icons) - 1))])
+        icon = '{0}'.format(icons[round(charge / 100 * (len(icons) - 1))])
     return str(round(charge)) + ' ' + icon
 
 
 def mail(path: str) -> str:
-    return str(len([listdir(p) for p in path])) + ' ' + ''
+    return str(len(ls(path))) + ' ' + ''
 
 
 def wifi() -> str:
@@ -62,8 +71,8 @@ def wifi() -> str:
         return '{0} [{1}]'.format(wif, sig)
 
 
-def audio() -> str:
-    return 'TODO'
+def paudio() -> str:
+    return popen('pamixer --get-volume').read().rstrip('\n')
 
 
 def date() -> str:
@@ -84,8 +93,7 @@ def mpd() -> str:
 if __name__ == "__main__":
     print(date())
     print("bat: {0}".format(battery()))
-    print("audio: {0}".format(audio()))
+    print("audio: {0}".format(paudio()))
     print("net: {0}".format(wifi()))
-    print("mail: {0}".format(mail(
-          [MAILPATH + '/' + p for p in listdir(MAILPATH) if p[0] != '.'])))
+    print("mail: {0}".format(mail(MAILPATH + '/new')))
     print(mpd())
